c88df3cf88dff500e517af38707f8933249399e1
OtaviioL <131923591+OtaviioL@users.noreply.github.com>
2023-11-24 16:39:17 -0300

    Add files via upload

---
+++ ConsultaController.cs
@@ -0,0 +1,20 @@
+﻿using LeagueOfShow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeagueOfShow.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        private readonly Contexto contexto;
+            public ConsultaController(Contexto context)
+        {
+            contexto = context;
+        }
+
+        public async Task<IActionResult> League()
+        {
+            var Leag = contexto.Artistas.ToList();
+            return View(Leag);
+        }
+    }
+}
